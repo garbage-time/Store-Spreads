59e37c7806afb5503dfeb11188745127af998b0b
Haddon <99365857+Haddon-Sandefur@users.noreply.github.com>
2024-04-20 16:30:19 -0400

    first commit

---
+++ odds_helper.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import datetime as dt
+import os
+
+# API keys
+ODDS_API_KEY          = os.getenv('ODDS_API_KEY')
+AWS_ACCESS_KEY_ID     = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+
+def normalize_odds_api_data(data_list: list):
+    data_normalized = []
+    for data in data_list:
+        unpack = pd.json_normalize(data, record_path=['sites'], meta=['id', 'sport_key', 'sport_nice', 'teams', 'commence_time', 'home_team', 'sites_count'], errors='ignore')
+        data_normalized.append(unpack)
+    df = pd.concat(data_normalized)
+    return df
+
+def split_odds_list_cols(data: pd.DataFrame):
+    df = data
+    cols_to_split = ['odds.spreads.odds', 'odds.spreads.points', 'teams']
+    
+    assert set(cols_to_split).issubset(df.columns), f'Columns {cols_to_split} not found in DataFrame'
+
+    # Split columns into new columns
+    df[['odds_away', 'odds_home']]     = df['odds.spreads.odds'].apply(pd.Series)
+    df[['points_away', 'points_home']] = df['odds.spreads.points'].apply(pd.Series)
+    df[['team_away', 'team_home']]     = df['teams'].apply(pd.Series)
+
+    # Drop the original columns
+    df.drop(columns=['teams', 'odds.spreads.odds', 'odds.spreads.points'], inplace=True)
+    return df
+
+def convert_unix_to_date(data: pd.DataFrame, date_cols_list: list):
+    df = data
+    for col in date_cols_list:
+        df[col] = pd.to_datetime(df[col], unit='s')
+        df[col] = df[col].dt.tz_localize('UTC').dt.tz_convert('US/Eastern')
+        df[col] = df[col].dt.strftime('%Y-%m-%d')
+    return df
+
+def aggregate_site_counts(data: pd.DataFrame):
+    df = data
+    
+    # Count the number of sites for each home team
+    df_grouped = df['home_team'].value_counts().reset_index()
+    df_grouped.columns = ['home_team', 'total_sites_count']
+    
+    # Merge the site counts back into the original DataFrame
+    df = pd.merge(df, df_grouped, on='home_team')
+    
+    # Drop old column and remove duplicates
+    df.drop(columns=['sites_count'], inplace=True)
+    df = df.drop_duplicates()
+
+    return df
---
+++ pull_spreads.py
@@ -0,0 +1,69 @@
+import nest_asyncio
+import odds_helper as oh
+import boto3
+import pandas as pd
+
+from oddsapi import OddsApiClient
+from botocore.exceptions import ClientError
+from io import StringIO
+
+# Needed for Odds API
+nest_asyncio.apply()
+
+# Connect to the Odds API and retreive spreads data:
+client = OddsApiClient(api_key=oh.ODDS_API_KEY)
+uk = client.retrieve_odds(sport_key='americanfootball_ncaaf', region='uk', mkt='spreads')
+us = client.retrieve_odds(sport_key='americanfootball_ncaaf', region='us', mkt='spreads')
+
+# Convert data to JSON for each market:
+uk_json = uk.json['data']
+us_json = us.json['data']
+
+# Normalize and concatenate the data:
+json_list = [uk_json, us_json]
+df = oh.normalize_odds_api_data(json_list)
+
+# Split columns with list elements:
+df = oh.split_odds_list_cols(df)
+
+# Convert Unix timestamps to date:
+date_cols = ['commence_time', 'last_update']
+df = oh.convert_unix_to_date(df, date_cols)
+
+# Aggregate site counts:
+df = oh.aggregate_site_counts(df)
+
+# Check that there are no duplicates in the DataFrame:
+assert not df.duplicated().any(), 'Duplicates found in DataFrame'
+
+# ===== Connect to S3 ============================================================
+s3 = boto3.client(
+    's3', 
+    aws_access_key_id=oh.AWS_ACCESS_KEY_ID, 
+    aws_secret_access_key=oh.AWS_SECRET_ACCESS_KEY
+)
+
+# Check if the bucket and flatfile exist:
+bucket = 'spreads-bucket'
+file = 'cfb_oapi_spreads.csv'
+
+try:
+    print(f'{file} exists... checking for updates')
+    s3_file = s3.get_object(Bucket=bucket, Key=file)
+    s3_df = pd.read_csv(s3_file['Body'])
+
+    # Combine and deduplicate the DataFrames:
+    combined_df = pd.concat([df, s3_df]).drop_duplicates().reset_index(drop=True)
+
+    # Push back to S3:
+    csv_buffer = StringIO()
+    combined_df.to_csv(csv_buffer, index=False)
+    s3.put_object(Bucket=bucket, Key=file, Body=csv_buffer.getvalue())
+
+except ClientError as e:
+    # If the file doesn't exist, upload the DataFrame
+    print(f'{file} not found')
+    print(f'Error: {e}')
+
+
+print('... data push complete')
